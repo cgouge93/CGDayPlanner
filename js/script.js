c38ba9871fed699729ae098172ee4788afe2bb7c
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-01-13 23:21:42 -0600

    finished for-in loop, created if/else statements for past/present/future

--- js/script.js
+++ js/script.js
@@ -18,4 +18,46 @@ $(document).ready(function() {
             .text(time);
         // adding class to event <td>
 		var tdEvent = $("<td>")
-			.addClass("textArea");
+            .addClass("textArea");
+       
+        // setting variable for current time
+		var thisTime;
+        
+        // conditional statements for past/present/future
+		if (moment(time, "h a").isSame(moment(), "hour")) {
+			thisTime = "present";
+		} else if (moment(time, "h a").isAfter(moment())) {
+			thisTime = "future";
+		} else if (moment(time, "h a").isBefore(moment())) {
+			thisTime = "past";
+		}
+
+		var eventText = $("<textarea>")
+            .addClass("description")
+
+            //adding classes for color based on conditional statement above
+            .addClass(thisTime)
+            
+            // setting attribute 
+			.attr("data-time", time)
+            .val(planner[time]);
+
+            //appending eventText <textarea> to tdEvent <td>
+		tdEvent.append(eventText);
+	
+        // creating <td> for save button and adding class
+		var tdSubmit = $("<td>").addClass("saveBtn"); 
+
+        // creating icons and class to determine what icon is used
+		var icon = $("<i>").addClass("far fa-save fa-2x");
+
+        // appending icon to tdSubmit <td>
+		tdSubmit.append(icon);
+		
+        // appending time, event, and submit buttons to table rows
+		tr.append(tdTime, tdEvent, tdSubmit);
+
+        // appending table rows to myPlanner div
+		$("#myPlanner").append(tr);
+		
+	}
